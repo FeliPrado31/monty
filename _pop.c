05342b5c853a740114906017e15ef99357641d7f
Feli <srgatoiscool@gmail.com>
2020-01-04 16:02:57 -0500

    fix pop

--- _pop.c
+++ _pop.c
@@ -4,6 +4,12 @@ void _pop(stack_t **head, unsigned int line_number)
 {
     stack_t *tmp = *head;
 
+    if (head == NULL || *head == NULL)
+    {
+        free(head);
+        exit(EXIT_FAILURE);
+    }
+
     *head = tmp->next;
     if (*head != NULL)
         (*head)->prev = NULL;
