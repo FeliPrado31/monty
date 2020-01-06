a048b1438b481c46a55807c5196759366f934840
Feli <srgatoiscool@gmail.com>
2020-01-05 21:49:10 -0500

    fix

--- parsingFile.c
+++ parsingFile.c
@@ -24,24 +24,11 @@ void parseFile(char *code, stack_t **stack, unsigned int line)
  */
 void readFile(char *file)
 {
-
-	struct stat Stat;
-	int check;
 	unsigned int i = 0;
 	ssize_t chr;
 	size_t size = 0;
 	stack_t *stack = NULL;
 
-	check = stat(file, &Stat);
-	if (check == 0)
-	{
-		if (!checkFile(file))
-		{
-			fprintf(stderr, "Error: Can't open file %s\n", file);
-			free(stack);
-			exit(EXIT_FAILURE);
-		}
-	}
 	b.f = fopen(file, "r");
 	if (!b.f)
 	{
@@ -53,22 +40,8 @@ void readFile(char *file)
 	{
 		i++;
 		parseFile(b.stream, &stack, i);
+
 	}
 	free_all(stack);
 	exit(EXIT_SUCCESS);
-}
-
-/**
- * checkFile - read the content of a monty file.
- * @file: The file  to a file
- * Return: Always 0.
- */
-int checkFile(char *file)
-{
-	int len = strlen(file);
-
-	len--;
-	if (file[len - 1] == '.' && file[len] == 'm')
-		return (1);
-	return (0);
 }
